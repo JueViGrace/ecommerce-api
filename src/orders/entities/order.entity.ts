66ddbeef39249164a0e504aa4efd37065a88c7cb
pablo castillo villalobos <danielzabalanava@gmail.com>
2023-12-21 19:38:02 -0400

    modified:   src/orders/entities/order.entity.ts

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,24 +1,53 @@
 import { User } from 'src/users/entities/user.entity';
-import { Entity, OneToOne } from 'typeorm';
+import { Column, Entity, OneToOne } from 'typeorm';
 
 @Entity({ name: 'pedido' })
 export class Order {
   @OneToOne(() => User, (user) => user.codigo)
   codigo: string;
 
+  @Column()
   fechamodifi: string;
+
+  @Column()
   kePedstatus: string;
+
+  @Column()
   ktiCodcli: string;
+
+  @Column()
   ktiCodven: string;
+
+  @Column()
   ktiCondicion: string;
+
+  @Column()
   ktiDocsol: string;
+
+  @Column()
   ktiFchdoc: string;
+
+  @Column()
   ktiNdoc: string;
+
+  @Column()
   ktiNegesp: string;
+
+  @Column()
   ktiNombrecli: string;
+
+  @Column()
   ktiNroped: string;
+
+  @Column()
   ktiStatus: string;
+
+  @Column()
   ktiTdoc: string;
+
+  @Column()
   ktiTipprec: number;
+
+  @Column()
   ktiTotneto: number;
 }
